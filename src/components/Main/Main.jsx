1a8be6860ee92edd21544d22877fe4f0a4114fe5
Ken Garcia Ortiz <garciaortizken@gmail.com>
2025-12-18 02:20:40 -0600

    Articles & preloader added to main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,18 +1,65 @@
 import "./Main.css";
+import { useState } from "react";
 import SearchForm from "../SearchForm/SearchForm";
-import About from "../About/About.jsx";
+import About from "../About/About";
+import Preloader from "../Preloader/Preloader";
+import NewsCard from "../NewsCard/NewsCard";
+import notFound from "../../images/notFound.svg";
 
-function Main() {
+function Main({ handleSearchSubmit, articles, isLoading, handleSaveArticle }) {
+  const [visibleArticles, setVisibleArticles] = useState(3);
+  const [hasSearched, setHasSearched] = useState(false);
+
+  const handleSearch = (e) => {
+    setHasSearched(true);
+    handleSearchSubmit(e);
+    setVisibleArticles(3);
+  };
+
+  const handleShowMore = () => setVisibleArticles((prev) => prev + 3);
+
   return (
     <main className="main">
       <section className="hero">
         <div className="section__content">
-          <SearchForm />
+          <SearchForm handleSearchSubmit={handleSearch} />
         </div>
       </section>
-      <section className="results">
-        <div className="section__content"></div>
-      </section>
+      {hasSearched && (
+        <>
+          {isLoading ? (
+            <section className="preloader-section">
+              <div className="section__content">
+                <Preloader />
+              </div>
+            </section>
+          ) : articles.length === 0 ? (
+            <section className="no-results">
+              <div className="section__content no-results__content">
+                <img src={notFound} alt="not found" className="no-results__img" />
+                <h1 className="no-results__title">Well, this is awkward</h1>
+                <p className="no-results__text">No results were found for that search</p>
+              </div>
+            </section>
+          ) : (
+            <section className="results">
+              <div className="section__content results__content">
+                <p className="results__text">Search results</p>
+                <ul className="results__list">
+                  {articles.slice(0, visibleArticles).map((item) => (
+                    <NewsCard key={item.url} article={item} handleSaveArticle={handleSaveArticle} />
+                  ))}
+                </ul>
+                {visibleArticles < articles.length && (
+                  <button className="results__show-more-btn" onClick={handleShowMore}>
+                    Show More
+                  </button>
+                )}
+              </div>
+            </section>
+          )}
+        </>
+      )}
       <section className="about">
         <div className="section__content">
           <About />
