dbe3779cfde87dec41fefa6b65ee4b1061184deb
Ken Garcia Ortiz <garciaortizken@gmail.com>
2025-12-18 02:14:11 -0600

    SearchForm section added

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,22 +1,35 @@
 import "./SearchForm.css";
+import { useState } from "react";
 
-function SearchForm() {
+function SearchForm({ handleSearchSubmit }) {
+  const [inputValue, setInputValue] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    handleSearchSubmit(inputValue);
+  }
+
   return (
     <div className="searchForm">
       <div className="searchForm__content">
         <h1 className="searchForm__title">What's going on in the world?</h1>
         <p className="searchForm__subtitle">
-          Find the latest news on any topic and save them in your personal
-          account.
+          Find the latest news on any topic and save them in your personal account.
         </p>
-        <div className="searchForm__field">
+        <form className="searchForm__field" onSubmit={handleSubmit}>
           <input
+            minLength="2"
             type="text"
             className="searchForm__field-input"
             placeholder="Enter topic"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            required
           />
-          <button className="searchForm__field-btn">Search</button>
-        </div>
+          <button type="submit" className="searchForm__field-btn">
+            Search
+          </button>
+        </form>
       </div>
     </div>
   );
