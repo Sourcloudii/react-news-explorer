63a71499bb4f29a8de21af4f3d33b86209000e3d
Ken Garcia Ortiz <garciaortizken@gmail.com>
2025-12-18 02:17:16 -0600

    Intro section added

---
+++ src/components/SavedNews/Intro.jsx
@@ -0,0 +1,31 @@
+import "./Intro.css";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../context/currentUserContent.js";
+
+export default function Intro({ articles }) {
+  const { user } = useContext(CurrentUserContext);
+
+  return (
+    <div className="intro">
+      <div className="intro__content">
+        <p className="intro__title">Saved articles</p>
+        {articles.length === 1 ? (
+          <h1 className="intro__subtitle">{user.name}, you have 1 saved article</h1>
+        ) : (
+          <h1 className="intro__subtitle">
+            {user.name}, you have {articles.length} saved articles
+          </h1>
+        )}
+        <p className="intro__keywords">
+          By keywords:{" "}
+          <span className="intro__span__keywords">
+            {articles
+              .map((article) => article.keyword)
+              .slice(0, 2)
+              .join(", ")}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
