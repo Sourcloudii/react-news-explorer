302a755767be0dd52465e88b7cfb89c71d855f70
Ken Garcia Ortiz <garciaortizken@gmail.com>
2026-01-15 23:31:48 -0600

    Added eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -5,10 +5,18 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+  {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
   },
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ]);
