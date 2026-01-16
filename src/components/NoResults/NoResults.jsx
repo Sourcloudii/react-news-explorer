74d3da9ea2a65de188704894b802eb950aa33645
Ken Garcia Ortiz <garciaortizken@gmail.com>
2026-01-15 23:33:27 -0600

    changed h1 to h3 tags

--- src/components/NoResults/NoResults.jsx
+++ src/components/NoResults/NoResults.jsx
@@ -5,7 +5,7 @@ export default function NoResults() {
   return (
     <div className="no-results__content">
       <img src={notFound} alt="not found" className="no-results__img" />
-      <h1 className="no-results__title">Well, this is awkward</h1>
+      <h3 className="no-results__title">Well, this is awkward</h3>
       <p className="no-results__text">No results were found for that search</p>
     </div>
   );
