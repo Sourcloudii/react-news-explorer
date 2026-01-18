0f9be208d487603606f2b691d6d09453dc12fbe5
Ken Garcia Ortiz <garciaortizken@gmail.com>
2026-01-18 14:05:47 -0600

    Added rules to eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -17,6 +17,8 @@ export default defineConfig([
   {
     rules: {
       "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react/no-unescaped-entities": "off",
     },
   },
 ]);
