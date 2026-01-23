a81c39383bda4e3aebcaa5467e5f46af7cb4fbe8
Ken Garcia Ortiz <garciaortizken@gmail.com>
2026-01-22 19:24:32 -0600

    declared useEffect import

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ModalWithForm from "../ModalWithForm/ModalWithForm.jsx";
 
 export default function LoginModal({ onClose, onOrClick, activeModal, handleLogin }) {
