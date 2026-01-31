920d4dcbd61ff3cb9a2e1aad5600e2c9ab285ea0
Ken Garcia Ortiz <garciaortizken@gmail.com>
2026-01-31 03:38:59 -0600

    BEM naming convention

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,11 +3,11 @@ import me from "../../images/Me-Profress.jpg";
 
 function About() {
   return (
-    <section className="about__container">
-      <img src={me} alt="Picture of creator" className="about__img" />
-      <div className="about__text-container">
-        <h2 className="about__title">About the author</h2>
-        <p className="about__subtitle">
+    <section className="about-section">
+      <img src={me} alt="Picture of creator" className="about-section__img" />
+      <div className="about-section__text-container">
+        <h2 className="about-section__title">About the author</h2>
+        <p className="about-section__subtitle">
           Hi! I'm Ken, a full-stack developer who studied at TripleTens coding bootcamp. I am
           fluent in HTML/CSS/JavaScript, along with React.js, Node.js and express.js. I am
           currently working on side projects including TripleTen's final proojcet, this news
